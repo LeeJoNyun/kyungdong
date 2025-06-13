f2763d6bdefd4f187ff2fdc0b7c15109dabcb674
kyeong2min <kyeong2min@gmail.com>
2025-06-13 09:22:17 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -3,6 +3,8 @@ const getAll = (target) => document.querySelectorAll(target);
 
 const preventDefaultAnchor = () => {
     const $links = getAll('a[href="#"]');
+    $links.forEach((link) => link.addEventListener('click', (e) => e.preventDefault()));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 const navi = () => {
@@ -10,7 +12,55 @@ const navi = () => {
 };
 const familySite = () => {};
 
-const comInit = () => {};
+const siteMap = () => {
+    const $sitemap = get('.sitemap');
+    const $bg = get('.bg');
+    const $allmenu = get('#header .util .all-menu');
+    const $close = get('.sitemap .close');
+
+    $allmenu.addEventListener('click', (e) => {
+        $sitemap.classList.add('on');
+        $bg.classList.add('on');
+    });
+    $close.addEventListener('click', (e) => {
+        $sitemap.classList.remove('on');
+        $bg.classList.remove('on');
+    });
+};
+
+const topBar = () => {
+    const $top = get('.top');
+    const $header = get('#header');
+
+    let ty = 0;
+
+    $top.addEventListener('click', (e) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    window.addEventListener('scroll', (e) => {
+        //window.scrollY , window.pageYOffset
+        // console.log(window.scrollY); -> 스크롤 위치 값 확인용
+        ty = window.scrollY;
+        if (ty > 200) {
+            $top.classList.add('on');
+        } else {
+            $top.classList.remove('on');
+        }
+
+        if (ty > 100) {
+            $header.classList.add('on');
+        } else {
+            $header.classList.remove('on');
+        }
+    });
+};
+
+const comInit = () => {
+    navi();
+    topBar();
+    siteMap();
+};
 
 (() => {
     preventDefaultAnchor();
